490cd3b63d79dbff1f3dd995373009ff3f1075f8
kimdohyung <pdohyung@gmail.com>
2023-07-21 20:25:58 +0900

    querydsl 공부를 위한 프로젝트 생성 및 설정, 적용 확인을 위한 테스트 구현

---
+++ src/main/java/study/querydsl/entity/HelloEntity.java
@@ -0,0 +1,15 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class HelloEntity {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+}
---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,38 @@
+package study.querydsl;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import study.querydsl.entity.HelloEntity;
+import study.querydsl.entity.QHelloEntity;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		HelloEntity helloEntity = new HelloEntity();
+		em.persist(helloEntity);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHelloEntity qHelloEntity = QHelloEntity.helloEntity;
+
+		HelloEntity result = query
+			.selectFrom(qHelloEntity)
+			.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(helloEntity);
+		Assertions.assertThat(result.getId()).isEqualTo(helloEntity.getId());
+	}
+
+}
